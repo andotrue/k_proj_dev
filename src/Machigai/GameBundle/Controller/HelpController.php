7c0963480bf9167964ca602c9a6282b3e3a96a0e
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2013-12-26 00:10:40 +0900

    ヘルプコントローラ追加

---
+++ src/Machigai/GameBundle/Controller/HelpController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Machigai\GameBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HelpController extends Controller
+{
+    public function indexAction()
+    {
+    }
+
+    public function howtoplayAction()
+    {
+    }
+
+    public function termsAction()
+    {
+    }
+
+    public function inquiryAction()
+    {
+    }
+
+}
