1bdcd124c442c8f55f89a5c4fdce74df09a1eebc
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2013-12-26 00:22:40 +0900

    ショップコントローラ追加

---
+++ src/Machigai/GameBundle/Controller/ShopController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Machigai\GameBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ShopController extends Controller
+{
+    public function indexAction()
+    {
+    }
+
+    public function wallpaperAction()
+    {
+    }
+
+    public function stampAction()
+    {
+    }
+
+    public function downloadAction()
+    {
+    }
+
+    public function errorAction()
+    {
+    }
+
+    public function confirmAction()
+    {
+    }
+
+}
