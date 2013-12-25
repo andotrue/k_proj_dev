0a8e1d59f58bf3ee0a94ee278c2b304af68d9c17
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2013-12-26 00:43:03 +0900

    Helpで各ページが表示されるように

--- src/Machigai/GameBundle/Controller/HelpController.php
+++ src/Machigai/GameBundle/Controller/HelpController.php
@@ -8,18 +8,22 @@ class HelpController extends Controller
 {
     public function indexAction()
     {
+	return $this->render('MachigaiGameBundle:Help:index.html.twig');
     }
 
     public function howtoplayAction()
     {
+	return $this->render('MachigaiGameBundle:Help:howtoplay.html.twig');
     }
 
     public function termsAction()
     {
+	return $this->render('MachigaiGameBundle:Help:terms.html.twig');
     }
 
     public function inquiryAction()
     {
+	return $this->render('MachigaiGameBundle:Help:inquiry.html.twig');
     }
 
 }
