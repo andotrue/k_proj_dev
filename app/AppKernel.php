99539245e0e34cb43646afee24f5a44aeb81c101
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2013-12-25 23:17:23 +0900

    認証バンドル登録

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Machigai\GameBundle\MachigaiGameBundle(),
+            new Machigai\AuthBundle\MachigaiAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Machigai/AuthBundle/MachigaiAuthBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Machigai\AuthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MachigaiAuthBundle extends Bundle
+{
+}
