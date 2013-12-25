3b395c4e0dbc7e1cb06f401f4c376e5cabcd9741
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2013-12-26 00:15:22 +0900

    設定コントローラ追加

---
+++ src/Machigai/GameBundle/Controller/SettingController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Machigai\GameBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SettingController extends Controller
+{
+    public function indexAction()
+    {
+    }
+
+    public function usernameAction()
+    {
+    }
+
+    public function completeAction()
+    {
+    }
+
+}
