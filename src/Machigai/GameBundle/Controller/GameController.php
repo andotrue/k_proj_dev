70523c218739295f63cbacb75240fb2dae492e0e
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2014-01-03 04:14:45 +0900

    GameController 追加

---
+++ src/Machigai/GameBundle/Controller/GameController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Machigai\GameBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GameController extends Controller
+{
+    public function selectAction()
+    {
+	return $this->render('MachigaiGameBundle:Game:select.html.twig');	
+    }
+
+    public function playAction()
+    {
+	return $this->render('MachigaiGameBundle:Game:play.html.twig');	
+    }
+
+    public function finishAction()
+    {
+	return $this->render('MachigaiGameBundle:Game:finish.html.twig');	
+    }
+
+    public function clearAction()
+    {
+	return $this->render('MachigaiGameBundle:Game:clear.html.twig');	
+    }
+
+    public function failAction()
+    {
+	return $this->render('MachigaiGameBundle:Game:fail.html.twig');	
+    }
+
+}
---
+++ src/Machigai/GameBundle/Tests/Controller/GameControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Machigai\GameBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GameControllerTest extends WebTestCase
+{
+    public function testSelect()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/game/select');
+    }
+
+    public function testPlay()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/game/play');
+    }
+
+    public function testFinish()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/game/finish');
+    }
+
+    public function testClear()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/game/clear');
+    }
+
+    public function testFail()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/game/fail');
+    }
+
+}
