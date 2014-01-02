e96cef1cb554a411b4ac2448ce6d9ebdde1269d5
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2014-01-03 04:56:08 +0900

    refs #125 2-04-1 設定 view 仮組み上げ

--- src/Machigai/GameBundle/Controller/SettingController.php
+++ src/Machigai/GameBundle/Controller/SettingController.php
@@ -8,14 +8,17 @@ class SettingController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('MachigaiGameBundle:Setting:index.html.twig');
     }
 
-    public function usernameAction()
+    public function nicknameAction()
     {
+        return $this->render('MachigaiGameBundle:Setting:nickname.html.twig');
     }
 
     public function completeAction()
     {
+        return $this->render('MachigaiGameBundle:Setting:complete.html.twig');
     }
 
 }
