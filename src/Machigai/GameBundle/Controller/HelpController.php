94e354d0dfa3b60708c13c996c7986e3de8a4289
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2014-01-04 06:55:39 +0900

    refs #121: 2-04-4-2 入力内容確認 ( /help/inquiry POST ) view 仮組み上げ

--- src/Machigai/GameBundle/Controller/HelpController.php
+++ src/Machigai/GameBundle/Controller/HelpController.php
@@ -35,12 +35,12 @@ class HelpController extends Controller
 	if( $form->get('confirm')->isClicked() ){
 	 $data = $form->getData();
 	 $form = $this->createFormBuilder()
-		 ->setAction($this->generateUrl('SettingNicknameregister'))
+		 ->setAction($this->generateUrl('HelpInquiryFormDo'))
 		 ->setMethod('POST')
-		 ->add('title', 'text',array('label'=>'件名'))
-		 ->add('email', 'email',array('label'=>'メールアドレス'))
-		 ->add('content', 'textarea',array('label'=>'お問い合わせ内容'))
-		 ->add('register', 'submit', array('label'=>'登録'))
+		 ->add('title', 'text',array('label'=>'件名', 'attr'=>array('disabled'=>'disabled')))
+		 ->add('email', 'email',array('label'=>'メールアドレス', 'attr'=>array('disabled'=>'disabled')))
+		 ->add('content', 'textarea',array('label'=>'お問い合わせ内容', 'attr'=>array('disabled'=>'disabled')))
+		 ->add('do', 'submit', array('label'=>'登録'))
 		 ->add('ammend','button',array('label'=>'修正', 'attr'=>array('onclick'=>'history.back()')))
 		 ->getForm();
 	 $form->setData($data);
@@ -50,7 +50,24 @@ class HelpController extends Controller
          return $this->render('MachigaiGameBundle:Help:form.html.twig', array('form' => $form->createView()) );
 	}
     }
-
+    
+    public function inquiryFormDoAction(){
+	$request = $this->getRequest();
+        $form = $this->createFormBuilder()
+		 ->add('title', 'text',array('label'=>'件名'))
+		 ->add('email', 'email',array('label'=>'メールアドレス'))
+		 ->add('content', 'textarea',array('label'=>'お問い合わせ内容'))
+		 ->add('do', 'submit', array('label'=>'登録'))
+		 ->getForm();
+	$form->handleRequest($request);		
+	 #TODO: Deal with nickname registration.
+	if ( $form->get('do')->isClicked() && $form->isValid()){
+		return $this->redirect($this->generateUrl('HelpInquiryFormComplete'));
+	}
+    }
+    public function inquiryFormCompleteAction(){
+	return $this->render('MachigaiGameBundle:Help:inquiryFormComplete.html.twig');
+    }
     public function inquiryAction()
     {
 	return $this->render('MachigaiGameBundle:Help:inquiry.html.twig');
