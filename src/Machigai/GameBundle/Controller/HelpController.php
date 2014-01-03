e966d1ca937cf8b0944a322ff2e64ffe7a22cfdf
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2014-01-04 06:25:50 +0900

    refs #120: 2-04-4-1 問い合わせフォーム ( /help/inquiry/form GET) view 仮組み上げ

--- src/Machigai/GameBundle/Controller/HelpController.php
+++ src/Machigai/GameBundle/Controller/HelpController.php
@@ -21,6 +21,36 @@ class HelpController extends Controller
 	return $this->render('MachigaiGameBundle:Help:terms.html.twig');
     }
 
+    public function formAction()
+    {
+	$request = $this->getRequest();
+        $form = $this->createFormBuilder()
+		 ->setMethod('GET')
+		 ->add('title', 'text',array('label'=>'件名'))
+		 ->add('email', 'email',array('label'=>'メールアドレス'))
+		 ->add('content', 'textarea',array('label'=>'お問い合わせ内容'))
+		 ->add('confirm', 'submit', array('label'=>'入力内容確認'))
+		 ->getForm();
+	$form->handleRequest($request);		
+	if( $form->get('confirm')->isClicked() ){
+	 $data = $form->getData();
+	 $form = $this->createFormBuilder()
+		 ->setAction($this->generateUrl('SettingNicknameregister'))
+		 ->setMethod('POST')
+		 ->add('title', 'text',array('label'=>'件名'))
+		 ->add('email', 'email',array('label'=>'メールアドレス'))
+		 ->add('content', 'textarea',array('label'=>'お問い合わせ内容'))
+		 ->add('register', 'submit', array('label'=>'登録'))
+		 ->add('ammend','button',array('label'=>'修正', 'attr'=>array('onclick'=>'history.back()')))
+		 ->getForm();
+	 $form->setData($data);
+         return $this->render('MachigaiGameBundle:Help:confirm.html.twig', array('form' => $form->createView()) );
+	}else{
+	// 通常のGETリクエスト
+         return $this->render('MachigaiGameBundle:Help:form.html.twig', array('form' => $form->createView()) );
+	}
+    }
+
     public function inquiryAction()
     {
 	return $this->render('MachigaiGameBundle:Help:inquiry.html.twig');
