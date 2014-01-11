ee40f11ed72a2a7f11a106b3091d06f848523a5f
白井 健太 <shirai.kenta@vareal.co.jp>
2014-01-11 17:27:42 +0900

    just commit to merge

--- src/Machigai/GameBundle/Controller/RankingController.php
+++ src/Machigai/GameBundle/Controller/RankingController.php
@@ -13,9 +13,17 @@ class RankingController extends Controller
           'SELECT r 
            FROM MachigaiGameBundle:Ranking r
            WHERE r.year = 2013
- 	   And r.month =12
+       	   And r.month =12
            ORDER BY r.level asc , r.rank asc')->getResult();
+/*  $ranking_user_this_month = $em->createQuery(
+          'SELECT u 
+           FROM MachigaiGameBundle:Ranking r
+           WHERE r.year = 2013
+           And r.month =12
+           ORDER BY r.level asc , r.rank asc')->getResult();
+*/
+//  var_dump($ranking_this_month);
+//  exit;
 	return $this->render('MachigaiGameBundle:Ranking:index.html.twig',array('ranking_this_month' => $ranking_this_month));
     }
-
 }
