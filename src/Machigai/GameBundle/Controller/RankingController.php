4149a019e9c2a61001e6d2a7252cccda56db403f
白井 健太 <shirai.kenta@vareal.co.jp>
2014-01-11 22:08:10 +0900

    tabs function in Shop

--- src/Machigai/GameBundle/Controller/RankingController.php
+++ src/Machigai/GameBundle/Controller/RankingController.php
@@ -9,26 +9,22 @@ class RankingController extends Controller
     public function indexAction()
     {
     $month = date('n');
+    $year = date('Y');
     if($month == 1){
+      $year = $year-1;
       $pre_month = 12;
     }else{
       $pre_month = $month-1;
     }  
     $ranking_this_month = $this->getDoctrine()
         ->getRepository('MachigaiGameBundle:Ranking')
-        ->findBy(array('month'=>$month));
+        ->findBy(array('month'=>$month,'year'=>$year));
     $ranking_previous_month = $this->getDoctrine()
         ->getRepository('MachigaiGameBundle:Ranking')
-        ->findBy(array('month'=>$pre_month));
-/*  
-	$em = $this->getDoctrine()->getManager();
-	$ranking_this_month = $em->createQuery(
-          'SELECT r 
-           FROM MachigaiGameBundle:Ranking r
-           WHERE r.year = 2013
-       	   And r.month =12
-           ORDER BY r.level asc , r.rank asc')->getResult();
-*/
+        ->findBy(array('month'=>$pre_month,'year'=>$year));
+    $aaa = $this->getDoctrine()
+        ->getRepository('MachigaiGameBundle:User')
+        ->findAll();
 	return $this->render('MachigaiGameBundle:Ranking:index.html.twig',array('ranking_this_month' => $ranking_this_month, 'ranking_previous_month' => $ranking_previous_month));
     }
 }
