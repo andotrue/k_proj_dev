8e2477cf18bdd7c967ecbc004e41ba48bf46ec96
Takuya Kondo <takuya.kondo@uzuuzu.jp>
2014-01-12 23:50:07 +0900

    表示の最適化

--- src/Machigai/GameBundle/Resources/public/js/game/cocos/cocos2d/main.js
+++ src/Machigai/GameBundle/Resources/public/js/game/cocos/cocos2d/main.js
@@ -14,36 +14,9 @@ var Application = cc.Application.extend({
 
         cc.EGLView.getInstance()._adjustSizeToBrowser();
         cc.EGLView.getInstance().adjustViewPort(true);
-        var screenSize = cc.EGLView.getInstance().getFrameSize();
-        var screenSize = cc.size(320,568);
-        var resourceSize = cc.size(640, 860);
-        var designSize = cc.size(720, 1280);
-
-        var searchPaths = [];
-        var resDirOrders = [];
-
-//        searchPaths.push("res");
-//        cc.FileUtils.getInstance().setSearchPaths(searchPaths);
-
-        var platform = cc.Application.getInstance().getTargetPlatform();
-        if (platform == cc.TARGET_PLATFORM.MOBILE_BROWSER) {
-//            resDirOrders.push("HD");
-        }
-        else if (platform == cc.TARGET_PLATFORM.PC_BROWSER) {
-            if (screenSize.height >= 800) {
-//                resDirOrders.push("HD");
-            }
-            else {
-                resourceSize = cc.size(1080, 1920);
-                designSize = cc.size(1080, 1920);
-//                resDirOrders.push("Normal");
-            }
-        }
-
-//        cc.FileUtils.getInstance().setSearchResolutionsOrder(resDirOrders);
 //        director.setContentScaleFactor(resourceSize.width / designSize.width);
-        cc.EGLView.getInstance().setDesignResolutionSize(designSize.width, designSize.height, cc.RESOLUTION_POLICY.SHOW_ALL);
-//        cc.EGLView.getInstance().resizeWithBrowserSize(true);
+        cc.EGLView.getInstance().setDesignResolutionSize(1080, 1920, cc.RESOLUTION_POLICY.SHOW_ALL);
+        cc.EGLView.getInstance().resizeWithBrowserSize(true);
 
         // turn on display FPS
         director.setDisplayStats(this.config['showFPS']);
