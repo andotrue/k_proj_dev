cc203f2bb25a0f453ad7982bf1629a3e684472a5
Masahiro Teramoto <teramoto.masahiro@vareal.co.jp>
2014-01-12 19:48:44 +0900

    Game Data  Download機能追加

--- src/Machigai/GameBundle/Controller/GameController.php
+++ src/Machigai/GameBundle/Controller/GameController.php
@@ -3,17 +3,37 @@
 namespace Machigai\GameBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class GameController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render('MachigaiGameBundle:Game:index.html.twig');   
+    }
     public function selectAction()
     {
-	return $this->render('MachigaiGameBundle:Game:select.html.twig');	
+    	return $this->render('MachigaiGameBundle:Game:select.html.twig');	
+    }
+    public function getPlayHistory()
+    {
+        $history = array('1' => array( '1' => array( 'qcode'=>'1' )));
+        return json_encode($history);
     }
 
     public function playAction()
     {
-	return $this->render('MachigaiGameBundle:Game:play.html.twig');	
+    	return $this->render('MachigaiGameBundle:Game:play.html.twig');	
+    }
+    public function downloadAction($level,$qcode,$type)
+    {
+        $types = array('xml' => '.xml', 'first' => '_1.png', 'second' => '_2.png');
+        $format = $types[$type];
+        $file = dirname(__FILE__).'/../Resources/questions/'.$level.'/'. $qcode . '/MS'. sprintf('%05d',$qcode). $format;
+
+        $response = new BinaryFileResponse($file);
+//        $response->prepare($request);
+        return  $response->send();
     }
 
     public function finishAction()
