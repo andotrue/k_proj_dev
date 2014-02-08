f017e8900fac3ab6284d682293f85cd498275c15
藤岡 拓己 <fujioka0729@gmail.com>
2014-02-09 00:43:43 +0900

    #320 【ゲーム】横向きにしたときに縦向きにしてもらうようメッセージを出す

--- src/Machigai/GameBundle/Resources/public/js/warning.js
+++ src/Machigai/GameBundle/Resources/public/js/warning.js
@@ -1,6 +1,9 @@
 $(function(){
 	//TODO:画像を差し込む
-	$("body").append('<img id="warningPopup" style="position:absolute;width:200px;height:150px;" src="{{ asset("/bundles/machigaigame/images/parts/.png")  }}" />');
+	$("body").append(
+			'<div id="warningPopup" style="position:absolute;width:100%;height:100%; background-color: #555;"' +
+			'><p style="color: #FFF; font-size: 12px; margin-top: 3em; text-align: center">このアプリケーションは縦向きでご利用ください</p></div>'
+	);
 	$('#warningPopup').hide();
 	$(window).bind("orientationchange",function(){
 		if(Math.abs(window.orientation) === 90){
