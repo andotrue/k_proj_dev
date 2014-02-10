0bb071cbb645b577d09f7eb5c78884dbd2da548c
藤岡 拓己 <fujioka0729@gmail.com>
2014-02-10 23:35:26 +0900

    #411 【ゲーム】iphone4で回転対応していない。

--- src/Machigai/GameBundle/Resources/public/js/warning.js
+++ src/Machigai/GameBundle/Resources/public/js/warning.js
@@ -1,11 +1,10 @@
 $(function(){
 	//TODO:画像を差し込む
 	$("body").append(
-			'<div id="warningPopup" style="position:absolute;width:100%;height:100%; background-color: #555;"' +
+			'<div id="warningPopup" style="display: none;position:absolute;width:100%;height:100%; background-color: #555;"' +
 			'><p style="color: #FFF; font-size: 12px; margin-top: 3em; text-align: center">このアプリケーションは縦向きでご利用ください</p></div>'
 	);
-	$('#warningPopup').hide();
-	$(window).bind("orientationchange",function(){
+	$(window).bind("load orientationchange",function(){
 		if(Math.abs(window.orientation) === 90){
 			$('#warningPopup').center();
 			$('#warningPopup').show();
