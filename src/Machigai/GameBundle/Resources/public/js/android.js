460d3a786f77def8a1ee59a945a6b90c6b512737
藤岡 拓己 <fujioka0729@gmail.com>
2014-02-11 12:32:41 +0900

    jsエラー

--- src/Machigai/GameBundle/Resources/public/js/android.js
+++ src/Machigai/GameBundle/Resources/public/js/android.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 	var agent = navigator.userAgent;
 	if(agent.search(/Android/) != -1){
 		$("a").each(function(){
-			if(  $(this).attr("href") && $(this).attr("href").search(/\/top$/) != -1 ){
+			if( $(this).attr("href") &&
+				$(this).attr("href").search(/\/top$/) != -1 &&
+				window["droid"]){
 				$(this).attr("href", "javascript:droid.goTop();");
 			}
 		});
