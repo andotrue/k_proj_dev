1fb6d8ceb5df3f1d916ec962ff43791244fae8aa
藤岡 拓己 <fujioka0729@gmail.com>
2014-03-01 14:02:49 +0900

    #174 【ランキング】ランキング登録バッチ処理

--- src/Machigai/GameBundle/Controller/RankingController.php
+++ src/Machigai/GameBundle/Controller/RankingController.php
@@ -2,8 +2,8 @@
 
 namespace Machigai\GameBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Machigai\GameBundle\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class RankingController extends BaseController
 {
@@ -27,4 +27,58 @@ class RankingController extends BaseController
         
 	return $this->render('MachigaiGameBundle:Ranking:index.html.twig',array('ranking_this_month' => $ranking_this_month, 'ranking_previous_month' => $ranking_previous_month));
     }
+
+	// 月初に実行されることを想定
+	public function summaryAction(){
+
+		$bonus_point = 500;
+		
+		$em = $this->getDoctrine()->getEntityManager();
+		//　トランザクション開始
+		$em->getConnection()->beginTransaction();
+		
+		try {
+			$month = date('n');
+			$year = date('Y');
+			if($month == 1){
+			  $pre_year = $year-1;
+			  $pre_month = 12;
+			}else{
+			  $pre_year = $year;
+			  $pre_month = $month-1;
+			}
+
+ 			// 先月のランキングを取得
+			$ranking_previous_month = $this->getDoctrine()
+				 ->getRepository('MachigaiGameBundle:Ranking')
+				 ->findBy(
+						 array(
+							'month'=>$pre_month,
+							'year'=>$pre_year,
+							'bonusPoint'=>null),
+						 array(
+							'rank'=>'ASC'),
+						 10);
+			
+			// 取得者にポイントを加算
+			foreach($ranking_previous_month as $data){
+				
+				$data->setBonusPoint($bonus_point);
+				$user = $data->getUser();
+				$user->setCurrentPoint($user->getCurrentPoint() + $bonus_point);
+				
+				$em->persist($data);
+				$em->persist($user);
+			}
+		
+			// トランザクション終了
+			$em->getConnection()->commit();
+			$em->flush();
+		} catch (ErrorException $ex){
+			$em->getConnection()->rollback();
+		}
+		
+		$em->close();
+		return new Response('ok');
+	}
 }
