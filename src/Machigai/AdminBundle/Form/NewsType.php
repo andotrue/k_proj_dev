ce88f6f10909edc293799f14864d80b348741e7b
藤岡 拓己 <fujioka0729@gmail.com>
2014-03-25 21:37:41 +0900

    #210 【管理機能】管理機能

---
+++ src/Machigai/AdminBundle/Form/NewsType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Machigai\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class NewsType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('startedAt')
+            ->add('endedAt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Machigai\GameBundle\Entity\News'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'machigai_gamebundle_News';
+    }
+}
