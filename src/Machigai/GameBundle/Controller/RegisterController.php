01e81d70da32ae78956c83bbf264a4351783d5fc
藤岡 拓己 <fujioka0729@gmail.com>
2014-04-12 15:47:16 +0900

    通常ログイン時のアンドロイドのみauIDログインにリダイレクトしない制約を除去

--- src/Machigai/GameBundle/Controller/RegisterController.php
+++ src/Machigai/GameBundle/Controller/RegisterController.php
@@ -188,15 +188,8 @@ class RegisterController extends BaseController
                     //auIDログインページへリダイレクト
                     return $this->redirect('https://auone.jp');
                 }else{
-                    //
-                    //return $this->render('MachigaiGameBundle:Android:afterAuIdLogin.html.twig', array('syncToken'=> $syncToken) );
-					$ua = $request->headers->get('User-Agent');
-					if(strpos($ua, "Android") == FALSE){
-						return $this->redirect($this->generateUrl('AuIdLogin'));
-					} else {
-	                    return $this->render('MachigaiGameBundle:Android:afterAuIdLogin.html.twig', array('syncToken'=> $syncToken) );
-//						return $this->redirect($this->generateUrl('Top'));
-					}
+					return $this->render('MachigaiGameBundle:Android:afterAuIdLogin.html.twig',
+							array('syncToken'=> $syncToken) );
                 }
         }
     }
