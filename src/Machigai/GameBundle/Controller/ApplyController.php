100da02dacfdaf5e3c1607f113978fd2bcef9bb1
藤岡 拓己 <fujioka0729@gmail.com>
2014-11-18 16:25:05 +0900

    add function apply

---
+++ src/Machigai/GameBundle/Controller/ApplyController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Machigai\GameBundle\Controller;
+
+use Machigai\GameBundle\Controller\BaseController;
+
+class ApplyController extends BaseController
+{
+    public function showAction($pageName)
+    {
+      // ログイン必須
+      $request = $this->get('request');
+      $cookies = $request->cookies;
+      $smartContract = $cookies->get('smartContract'); 
+      if(!$this->DEBUG && (empty($smartContract) || $smartContract != "true") ){
+
+        $session = $request->getSession();
+        $session->set('puzzlelp_access', 'true');
+        return $this->redirect($this->generateUrl('response_token'));
+      }
+
+      return $this->render('MachigaiGameBundle:ApplyShow:' . $pageName . '.twig');
+    }
+
+    public function sendDataAction()
+    {
+      // ログイン必須
+      $request = $this->get('request');
+      $cookies = $request->cookies;
+      $smartContract = $cookies->get('smartContract'); 
+      if(!$this->DEBUG && (empty($smartContract) || $smartContract != "true") ){
+
+        $session = $request->getSession();
+        $session->set('puzzlelp_access', 'true');
+        return $this->redirect($this->generateUrl('response_token'));
+      }
+      
+      // POSTデータよりメール本文の作成
+      // ルールとして
+      // [name][改行]
+      // [vale][改行]
+      // [改行]
+      // となるようにする。つまりnameは日本語になる
+      // 
+      // デフォルトパラメータ
+      // title : 応募タイトル
+      
+      $title = $request->request->get('title');
+      
+      $content = $this->createMailContent($request->request);
+      
+      $body = $content;
+      
+      // メール送信処理
+			$message = \Swift_Message::newInstance()
+	        ->setSubject('【まちがいさがし放題】応募_' . $title)
+	        ->setFrom('support@machigai.puzzle-m.net')
+	        ->setTo('support@machigai.puzzle-m.net')
+	        ->setBody($body);
+	        
+	        $this->get('mailer')->send($message);
+      
+      // 
+      return $this->render('MachigaiGameBundle:Apply:complete.html.twig');
+      
+    }
+    
+    function createMailContent($request){
+      
+      $datas = $request->request->all();
+      
+      $body = "";
+      foreach($datas as $key => $val){
+        
+        $body = $body . $key . "\n";
+        $body = $body . "　" . $val . "\n";
+        $body = $body . "\n";
+      }
+      
+      return $body;
+    }
+    
+}
--- src/Machigai/GameBundle/Controller/BaseController.php
+++ src/Machigai/GameBundle/Controller/BaseController.php
@@ -12,8 +12,8 @@ class BaseController extends Controller
 {
     /** DEBUGモード　*/
     public $MODE = "DEBUG";
-    public $DEBUG = false;
-	public $AUID_DEBUG = false;
+    public $DEBUG = true;
+	public $AUID_DEBUG = true;
 
 	public function getUser()
 	{
