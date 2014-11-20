cead52bdd91e6aa1db9e6be92a1a8e716c7870fd
藤岡 拓己 <fujioka0729@gmail.com>
2014-11-20 17:14:28 +0900

    トップに20pxのスペースを開ける処理

--- src/Machigai/GameBundle/Resources/public/js/common.js
+++ src/Machigai/GameBundle/Resources/public/js/common.js
@@ -7,7 +7,15 @@
 $(function(){
 	var userAgent = window.navigator.userAgent.toLowerCase();
 	if (userAgent.indexOf('/spass-app/') != -1){
+		// 全てのページの先頭に20pxの空白を挿入
 		$("body").prepend("<div style='width: 100%; height: 20px'>&nbsp;</div>");
+		
+		$("*").each(function(){
+			if($(this).css("position") == "absolute"){
+				$(this).css("top", parseInt($(this).css("top"),10) + 20);
+			}
+		});
+		$("footer").css("padding-top", "40px");
 	}
 });
 
