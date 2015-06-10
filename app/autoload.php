5bf3c7136d1656faf228067cd53a64b7a3026505
Atsushi Ando <andotrue@gmail.com>
2015-06-11 00:27:29 +0900

    test symfony performance up

--- app/autoload.php
+++ app/autoload.php
@@ -1,5 +1,4 @@
 <?php
-
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
@@ -7,7 +6,24 @@ use Composer\Autoload\ClassLoader;
  * @var ClassLoader $loader
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
-
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
+
+/*
+require __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';//add
+require __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';//add
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Composer\Autoload\ClassLoader;
+
+use Symfony\Component\ClassLoader\ApcUniversalClassLoader;//add
+
+
+$loader2 = require __DIR__.'/../vendor/autoload.php';
+$loader = new ApcUniversalClassLoader('');//add
+
+AnnotationRegistry::registerLoader(array($loader2, 'loadClass'));
+
+return $loader;
+*/
